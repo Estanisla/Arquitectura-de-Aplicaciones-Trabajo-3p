c0539378cb8feffd9a92d43dd06630fc7d30595d
ByGouDex <sixtodiego16@gmail.com>
2026-06-24 19:39:50 -0500

    Delete backend/src/modules/vendor-dashboard/vendor-dashboard/vendor-dashboard directory

--- backend/src/modules/vendor-dashboard/vendor-dashboard/vendor-dashboard/vendor-dashboard.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { requireVendorSession } from "../../shared/requireVendorSession.js";
-import { vendorDashboardController } from "./vendor-dashboard.controller.js";
-
-export const vendorDashboardRouter = Router();
-
-vendorDashboardRouter.get("/me", requireVendorSession, vendorDashboardController.getMyProfile);
